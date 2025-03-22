c2358fbf6ce2407f118d1977acc3e45ae78ea11a
VladTomici14 <vladtomici@gmail.com>
2025-03-22 23:39:05 +0200

    Added the first solutions (need checks)

---
+++ lab4/ex2.m
@@ -0,0 +1,16 @@
+% ----------------
+% ----- EX 2 -----
+% ----------------
+
+% a) 
+% ----- defining the corresponding matrices -----
+A_a = [4 3 -1; -1 1 1; 1 0 3; 4 4 3]; 
+B_a = [2; 0; -1; 1];
+
+rank_A_a = rank(A_a);
+fprintf('a) Rank of coefficient matrix: %d\n', rank_A_a);
+
+% b) 
+% ----- defining the corresponding matrices -----
+A_b = [-6 8 -1; 2 -3 0];
+B_b = [-5; 7];
